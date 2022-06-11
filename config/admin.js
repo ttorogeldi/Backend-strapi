bd4ad874f930b2c1261cdef0759a245582939134
Torogeldi Tologonov <ttorogeldi@gmail.com>
2022-06-11 23:24:11 +0600

    add

--- config/admin.js
+++ config/admin.js
@@ -3,6 +3,6 @@ module.exports = ({env}) => ({
     secret: env('ADMIN_JWT_SECRET'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT','2959044c643a1858b8da3a400d0454bd17f6975a0af2bfe4a9d287987a24ae1dc60febb6b7b0e11d85e99d23bab574e4739c094bf8b182fcb431cd4a1f9e600afce617db7c13bada6c9d03c12f854c76e314ee865f45992278fe5b33492cf02643aebf601a78b498c0413880a1d268f3e08d69554c06f481c8d74e53517bb657'),
   },
 });
--- config/database.js
+++ config/database.js
@@ -1,11 +1,15 @@
 const path = require('path');
-
-module.exports = ({ env }) => ({
+module.exports = ({env}) => ({
   connection: {
-    client: 'sqlite',
+    client: 'postgres',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env('DATABASE_HOST', 'localhost'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'strapi'),
+      user: env('DATABASE_USERNAME', 'postgres'),
+      password: env('DATABASE_PASSWORD', 'Tohabratan'),
+      schema: env('DATABASE_SCHEMA', 'public'), // Not required
     },
-    useNullAsDefault: true,
+    debug: false,
   },
 });
